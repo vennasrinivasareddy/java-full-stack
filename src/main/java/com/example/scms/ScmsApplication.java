13e579f36259ab35a686ec0c44448d5063930c28
venna srinivasa reddy <2100032311@kluniversity.in>
2026-03-12 11:25:05 +0530

    fixed jpa and deployment

--- src/main/java/com/example/scms/ScmsApplication.java
+++ src/main/java/com/example/scms/ScmsApplication.java
@@ -1,14 +1,15 @@
 package com.example.scms;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.scms")
 public class ScmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScmsApplication.class, args);
+    }
 
 }
